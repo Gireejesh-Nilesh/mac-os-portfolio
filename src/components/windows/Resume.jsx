e6fa314d76eadaf52e49f7056b9fe13f59572bf8
Nilesh043 <23p65a0508@vbithyd.ac.in>
2026-01-27 20:23:15 +0530

    Updated Resume

--- src/components/windows/Resume.jsx
+++ src/components/windows/Resume.jsx
@@ -1,13 +1,25 @@
 import "./resume.scss";
 import MacWindow from "./MacWindow";
 
-const Resume = ({ windowName, windowState, isNewlyOpened, setActiveWindow, setWindowsState }) => {
+const Resume = ({
+  windowName,
+  windowState,
+  isNewlyOpened,
+  setActiveWindow,
+  setWindowsState,
+}) => {
   return (
-    <MacWindow windowName={windowName} windowState={windowState} isNewlyOpened={isNewlyOpened} setActiveWindow={setActiveWindow} setWindowsState={setWindowsState}>
+    <MacWindow
+      windowName={windowName}
+      windowState={windowState}
+      isNewlyOpened={isNewlyOpened}
+      setActiveWindow={setActiveWindow}
+      setWindowsState={setWindowsState}
+    >
       <div className="resume-window">
         <embed
           title="Resume"
-          src="/deloitte-nilesh.pdf"
+          src="/Resume_Gireejesh_Nilesh.pdf"
           width="100%"
           height="100%"
           style={{ border: "none" }}
